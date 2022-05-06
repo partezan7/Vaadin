78cdcac97c953cc2d99796ebb8bfa546ae94010a
Denis Savitskii <partezan7@rambler.ru>
2022-05-06 20:09:54 +0300

    Next iteration

--- src/main/java/com/example/app/MyView.java
+++ src/main/java/com/example/app/MyView.java
@@ -1,92 +1,65 @@
 package com.example.app;
 
-import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.board.Board;
 import com.vaadin.flow.component.board.Row;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dnd.DragSource;
 import com.vaadin.flow.component.dnd.DropTarget;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.Route;
 
-/**
- * A Designer generated component for the my-view template.
- * <p>
- * Designer will add and remove fields with @Id mappings but
- * does not overwrite or otherwise change this file.
- */
-@Tag("my-view")
-@JsModule("./my-view.ts")
 @Route("")
-public class MyView extends LitTemplate {
+public class MyView extends HorizontalLayout {
     int counter = 0;
     private Button button;
-    @Id("verticalLayout")
     private VerticalLayout verticalLayout;
-    @Id("horizontalLayout")
-    private HorizontalLayout horizontalLayout;
-    @Id("board")
     private Board board;
 
-    /**
-     * Creates a new MyView.
-     */
     public MyView() {
+        init();
+    }
+
+    private void init() {
+        setSizeFull();
+
+        verticalLayout = new VerticalLayout();
+        verticalLayout.setHeight(100.0f, Unit.PERCENTAGE);
+        verticalLayout.setWidth(10.0f, Unit.PERCENTAGE);
+
         button = createButton();
         verticalLayout.add(button);
+
+        board = new Board();
+        board.setSizeFull();
+
+        add(verticalLayout, board);
+        dropButton(board);
     }
 
     private void dragButton(Button button) {
         DragSource<Button> dragSource = DragSource.create(button);
-        dropButton(board);
         dragSource.addDragEndListener(event -> {
-            verticalLayout.removeAll();
-            verticalLayout.add(createButton());
         });
     }
-
-//    private void dropButton(Board board) {
-//        DropTarget<Board> dropTarget = DropTarget.create(board);
-//        dropTarget.addDropListener(event -> {
-//            Row row0 = new Row();
-//            Div div0 = new Div();
-//            Div div1 = new Div();
-//            Div div2 = new Div();
-//            Div div3 = new Div();
-//            Button button = (Button) event.getDragSourceComponent().get();
-//            button.setSizeFull();
-//
-//            div0.add(button);
-//            row0.add(div0, div1, div2, div3);
-//            board.add(row0);
-//        });
-//    }
 
     private void dropButton(Board board) {
         DropTarget<Board> dropTarget = DropTarget.create(board);
         dropTarget.addDropListener(event -> {
-            if (counter == 1) {
-                Row row0 = new Row();
-                row0.setSizeFull();
-                Div div0 = new Div();
-                Button button = (Button) event.getDragSourceComponent().get();
-                button.setSizeFull();
-
-                div0.add(button);
-                row0.add(div0);
-                board.add(row0);
-            }
+            verticalLayout.add(createButton());
+            Row row0 = new Row();
+            Button button = (Button) event.getDragSourceComponent().get();
+            button.setSizeFull();
+            row0.add(button);
+            board.add(row0);
         });
     }
 
     private Button createButton() {
         counter++;
-        Button newButton = new Button(String.valueOf(counter));
+        Button newButton = new Button(String.valueOf(counter),
+                buttonClickEvent -> button.setHeight(30, Unit.PERCENTAGE));
         dragButton(newButton);
         return newButton;
     }
