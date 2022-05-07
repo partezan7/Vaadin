8c9b6c1d0623a092910e933b15d5fdeb4cf0eea7
Denis Savitskii <partezan7@rambler.ru>
2022-05-07 16:04:46 +0300

    Next iteration

--- src/main/java/com/example/app/MyView.java
+++ src/main/java/com/example/app/MyView.java
@@ -2,7 +2,6 @@ package com.example.app;
 
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.board.Board;
-import com.vaadin.flow.component.board.Row;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dnd.DragSource;
 import com.vaadin.flow.component.dnd.DropTarget;
@@ -13,8 +12,12 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class MyView extends HorizontalLayout {
     int counter = 0;
-    private Button button;
     private VerticalLayout verticalLayout;
+    private VerticalLayout verticalLayout1;
+    private VerticalLayout verticalLayout2;
+    private HorizontalLayout horizontalLayout;
+    private VerticalLayout verticalLayout3;
+    private VerticalLayout verticalLayout4;
     private Board board;
 
     public MyView() {
@@ -23,33 +26,50 @@ public class MyView extends HorizontalLayout {
 
     private void init() {
         setSizeFull();
-//
         verticalLayout = new VerticalLayout();
         verticalLayout.setHeight(100.0f, Unit.PERCENTAGE);
         verticalLayout.setWidth(150.0f, Unit.PIXELS);
-//
         Button buttonGenerator = new Button("Content Generator");
+        dragButton(buttonGenerator);
         verticalLayout.add(buttonGenerator);
-
-        buttonGenerator.addClickListener(buttonClickEvent ->
-                addContent());
-
+        buttonGenerator.addClickListener(buttonClickEvent -> addContent());
         add(verticalLayout);
     }
 
     private void addContent() {
-        if (counter==0){
-            VerticalLayout verticalLayout1 = new VerticalLayout();
-            verticalLayout1.add(createButton());
-            add(verticalLayout1);
-        }else{
-            if (counter==1){
-                VerticalLayout verticalLayout2 = new VerticalLayout();
+        switch (counter) {
+            case 0:
+                verticalLayout1 = new VerticalLayout();
+                verticalLayout1.add(createButton());
+                add(verticalLayout1);
+                break;
+            case 1:
+                verticalLayout2 = new VerticalLayout();
                 verticalLayout2.add(createButton());
                 add(verticalLayout2);
-            }
+                break;
+            case 2:
+                verticalLayout2.add(createButton());
+                break;
+            case 3:
+                verticalLayout3 = new VerticalLayout();
+                verticalLayout3.add(createButton());
+                add(verticalLayout3);
+                break;
+            case 4:
+                horizontalLayout = new HorizontalLayout();
+                horizontalLayout.add(this.getComponentAt(1), this.getComponentAt(2));
+                horizontalLayout.setSizeFull();
+                verticalLayout4 = new VerticalLayout();
+                verticalLayout4.add(createButton());
+                verticalLayout4.add(horizontalLayout);
+                addComponentAtIndex(1, verticalLayout4);
+                break;
+            case 5:
+                horizontalLayout.addComponentAtIndex(2, horizontalLayout.getComponentAt(1));
+                horizontalLayout.addComponentAtIndex(1, createButton());
+                break;
         }
-
     }
 
     private void dragButton(Button button) {
@@ -58,23 +78,19 @@ public class MyView extends HorizontalLayout {
         });
     }
 
-    private void dropButton(Board board) {
-//        DropTarget<Board> dropTarget = DropTarget.create(board);
-//        dropTarget.addDropListener(event -> {
-//            verticalLayout.add(createButton());
-//            Row row0 = new Row();
-//            Button button = (Button) event.getDragSourceComponent().get();
-//            button.setSizeFull();
-//            row0.add(button);
-//            board.add(row0);
-//        });
+    private void dropButton(Button button) {
+        DropTarget<Button> dropTarget = DropTarget.create(button);
+        dropTarget.addDropListener(event -> {
+            Button newButton = createButton();
+            Button buttonOld = event.getComponent();
+            buttonOld.setText(newButton.getText());
+        });
     }
 
     private Button createButton() {
         counter++;
-        Button newButton = new Button(String.valueOf(counter),
-                buttonClickEvent -> button.setHeight(30, Unit.PERCENTAGE));
-        dragButton(newButton);
+        Button newButton = new Button(String.valueOf(counter));
+        dropButton(newButton);
         newButton.setSizeFull();
         return newButton;
     }
